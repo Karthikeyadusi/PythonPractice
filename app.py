ef3c6b28667a545d58f932c8631aa4ac1739de9f
Karthikeya Dusi <dvskartikeya@gmail.com>
2025-10-01 10:53:38 +0530

    learning python practically

---
+++ app.py
@@ -0,0 +1,9 @@
+#print("Hello, World")
+
+#celsius -> fahrenheit
+
+'''celsius = float(input("Enter temperature in celsius: "))
+fahrenheit = ((9/5)*celsius) + 32
+print("Temperature in fahrenheit is: ", fahrenheit)
+'''
+
