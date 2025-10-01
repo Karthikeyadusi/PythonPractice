37e6ed439099bdff2345805a20bae9e562e4eb65
Karthikeya Dusi <dvskartikeya@gmail.com>
2025-10-01 11:44:08 +0530

    practiced till string manipulations

--- app.py
+++ app.py
@@ -2,8 +2,39 @@
 
 #celsius -> fahrenheit
 
+#Write a program to convert temperature from Celsius to Fahrenheit. Input temperature in Celsius as a float, output the Fahrenheit value.
 '''celsius = float(input("Enter temperature in celsius: "))
 fahrenheit = ((9/5)*celsius) + 32
 print("Temperature in fahrenheit is: ", fahrenheit)
 '''
+
+#Create a program that asks for two numbers and prints their sum.
+
+'''a = int(input("Enter no1: "))
+b = int(input("Enter no2: "))
+print("Their sum is : ",a+b)'''
+
+
+#Write a program that takes two integers and returns True if one is a multiple of the other.
+
+'''a = int(input("enter no1: "))
+b = int(input("enter no2: "))
+
+if a%b==0 or b%a==0:
+    print("True")
+else:
+    print("False")'''
+
+#Write a program to check if a given string is a palindrome.
+
+'''string = input("Enter a string : ")
+
+reversed_string = string[::-1]
+
+if reversed_string == string:
+    print("its a palindrome")
+else:
+    print("not a palindrome")'''
+
+
 
